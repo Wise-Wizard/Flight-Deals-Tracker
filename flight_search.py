1715acc4bf12c18819898a5706c89046295065ee
Wise-Wizard <saransh.shankar@gmail.com>
2023-08-16 18:49:37 +0530

    Finished Flight Project

--- flight_search.py
+++ flight_search.py
@@ -1,7 +1,54 @@
 import requests
-TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+from flight_data import FlightData
+from dotenv import load_dotenv
+import os
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
+TEQUILA_ENDPOINT_V2 = "https://api.tequila.kiwi.com/v2"
+load_dotenv(".env")
+TEQUILA_API_KEY = os.getenv("TEQUILA_API_KEY")
+
+
 class FlightSearch:
     def get_destination_code(self, city):
-        code = "Testing"
-        return code
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city, "location_types": "city"}
+        response = requests.get(url=location_endpoint,
+                                params=query, headers=headers)
+        results = response.json()["locations"]
+        return results[0]["code"]
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        location_endpoint = f"{TEQUILA_ENDPOINT_V2}/search"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "INR"
+        }
+        response = requests.get(url=location_endpoint,
+                                headers=headers, params=query)
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: INR {flight_data.price}")
+        return flight_data
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from datetime import datetime, timedelta
 from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+ORIGIN_CITY_IATA = "BLR"
 data_manager = DataManager()
+flight_search = FlightSearch()
+notification = NotificationManager()
 sheet_data = data_manager.get_destination_data()
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+for destination in sheet_data:
+    flight = flight_search.check_flights(origin_city_code=ORIGIN_CITY_IATA, destination_city_code=destination["iataCode"], from_time=tomorrow, to_time=six_month_from_today)
+    if flight is not None and flight.price < destination['lowestPrice']:
+        notification.send_sms(message=f"Low price alert! Only INR {flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
